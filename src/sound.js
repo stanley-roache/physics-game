845cb6ec671a376b04601bbcd5702ba684e8bff0
stanley-roache <stan.roache@gmail.com>
2019-06-24 12:31:41 +1200

    remove comment for clarity

--- src/sound.js
+++ src/sound.js
@@ -6,7 +6,6 @@ for (let i = 0; i < audioDepth; i++) {
   sounds.push(new Audio("sounds/bubble_pop.mp3"))
 }
 
-// when blobs collide play this sound
 export function playSound() {
   soundIndex = (soundIndex+1)%audioDepth;
   sounds[soundIndex].play();
